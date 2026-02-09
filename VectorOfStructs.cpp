f1b7e8f5667425789dde18141a9d67723cb1d9e6
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-08 20:12:53 -0800

    update

--- VectorOfStructs.cpp
+++ VectorOfStructs.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+
 //defining struct for holding RGB hue values
 struct Color{
     int red;
@@ -10,13 +11,26 @@ struct Color{
     int blue;
 };
 
-int main(){
+const int max = 50, min = 25;
 
+int main(){
+    srand(time(0));
+    
     //vector to hold data from temp struct
     vector<Color> color;
-    Color temp = {232, 324, 2};
-    color.push_back(temp);
     
+    //generating random number for # of Color objects
+    int rnd = rand() % (max - min + 1) + min;
+
+    for (int i = 0, i < rnd; i++){
+        //temp struct to push into vector
+        int red = rand() % 300;
+        int green = rand() % 300;
+        int blue = rand() % 300;
+        Color temp = {red, green, blue};
+        color.push_back(temp);
+    }
+
 
     return 0;
 }
